10ffc94400f5cd327d1bbc8f39dc6656b593500b
DIVYA500-s <divyanshiprajapati263@gmail.com>
2026-04-08 16:06:46 +0530

    Add tota revenue  analysis query with business insights.

--- query 2.sql
+++ query 2.sql
@@ -1,5 +1,10 @@
--- Calculate the total revenue generated from pizza sales.
-select  round(sum(quantity*price),2) AS total_revenue
-from order_details
-LEFT JOIN pizzas 
+-- Business Question: What is the total revenue generated from pizza sales?
+-- Insight: Helps measure overall business performance and track revenue growth.
+
+SELECT ROUND(SUM(quantity * price), 2) AS total_revenue
+FROM order_details
+LEFT JOIN pizzas
 ON pizzas.pizza_id = order_details.pizza_id;
+
+--Total Revenue  
+  817860.05
